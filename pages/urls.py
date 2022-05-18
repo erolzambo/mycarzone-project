1a7097c83d37ea97cb49a34375f50332384acf0b
erolzambo <73480244+erolzambo@users.noreply.github.com>
2022-05-18 19:28:01 +0800

    git status checked

--- pages/urls.py
+++ pages/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from pages import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
-    path('', views.about, name='about'),
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
 ]
